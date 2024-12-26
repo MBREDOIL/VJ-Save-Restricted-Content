87f6a619b145593bfe38f486bbd5182464071885
MBREDOIL <159939043+MBREDOIL@users.noreply.github.com>
2024-12-26 15:08:30 +0530

    config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "24143260")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "f65e566d06fc196655413aaf09c9e6e7"))
+API_ID = int(os.environ.get("API_ID", "24143260"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "f65e566d06fc196655413aaf09c9e6e7")
 
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "6106998329"))
